f303e7e32d2968606be2f157fb51b6fa96508540
Nicholas Cannon <nicholascannon1@gmail.com>
2018-12-29 19:28:53 +0800

    Added pagination

--- public/js/app.js
+++ public/js/app.js
@@ -1,23 +1,50 @@
 var TOKEN = localStorage.getItem('xsffncToken');
+var PAGE = 1;
 
 function getContacts(page) {
   var cb = $('#contactsBoard');
+
+  // Pagination highlighting
+  if (page > 1) {
+    $('#prevLi').removeClass('disabled');
+  } else if (page === 1) {
+    $('#prevLi').addClass('disabled');
+  }
 
   $.ajax({
     url: 'http://localhost:8000/api/contact/page/'+page,
     headers: { 'Authorization':  TOKEN},
     success: function (data, status) {
       if (data.contacts.length === 0) {
-        // No contact objects
-        
+        $('#noMsg').show();
+        $('#nextLi').addClass('disabled');
+      } else {
+        $('#noMsg').hide();
+        $('#nextLi').removeClass('disabled');
       }
     },
     error: function (data, status) {
 
     }
-  })
+  });
 }
 
 $('document').ready(function() {
-  getContacts(1);
+  getContacts(PAGE);
+
+  // Button Listeners
+  $('#logoutBtn').click(function() {
+    localStorage.removeItem('xsffncToken');
+    window.location.replace('/login');
+  });
+  $('#prevPage').click(function() { 
+    PAGE--;
+    getContacts(PAGE);
+    return false;
+  });
+  $('#nextPage').click(function() { 
+    PAGE++;
+    getContacts(PAGE);
+    return false;
+  });
 });
